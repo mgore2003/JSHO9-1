d382fee082ba8228b28463063fb8cf1a4a70f93d
mgore2003 <142228045+mgore2003@users.noreply.github.com>
2024-04-29 08:16:35 -0400

    Add files via upload

---
+++ project09-01b.js
@@ -0,0 +1,21 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Project 09-01
+
+      Project to read field values from a query string
+      Author: 
+      Date:   
+
+      Filename: project09-01b.js
+*/
+let query = location.search.slice(1);
+query = query.replace(/\+/g, " ");
+query = decodeURIComponent(query);
+let cardFields = query.split(/&/g);
+for (let items of cardFields) {
+	let nameValue = items.split(/=/);
+	let name = nameValue[0];
+	let value = nameValue[1];
+	document.getElementById(name).textContent = value; 
+}
